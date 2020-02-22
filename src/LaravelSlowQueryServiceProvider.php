a68b46c273ae989801e39bc749adcdb0c5a02967
Mark van Eijk <mark@vaneijk.co>
2020-02-22 09:30:16 +0100

    slow query service provider

--- src/LaravelSlowQueryServiceProvider.php
+++ src/LaravelSlowQueryServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Vormkracht10\LaravelSlowQuery;
 
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\ServiceProvider;
+use Vormkracht10\LaravelSlowQuery\Events\FoundSlowQuery;
+use Vormkracht10\LaravelSlowQuery\Notifications\SlowQueryDetected;
 
 class LaravelSlowQueryServiceProvider extends ServiceProvider
 {
@@ -11,13 +14,33 @@ class LaravelSlowQueryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->setupEvents($this->app->events);
     }
 
-    /**
-     * Register the application services.
-     */
     public function register()
     {
+    }
+
+    public function setupEvents($events)
+    {
+        if (class_exists(QueryExecuted::class)) {
+            $events->listen(QueryExecuted::class, function (QueryExecuted $query) {
+                $query = $this->makeQuery($query->sql, $query->bindings, $query->time, $query->connectionName);
+
+                if ($this->slowQueryCheck($query)) {
+                    event(FoundSlowQuery($query));
+                }
+            });
+        }
+    }
+
+    public function makeQuery($sql, $bindings, $time, $connectionName)
+    {
+        return new Query($sql, $bindings, $time, $connectionName);
+    }
+
+    public function slowQueryCheck(Query $query)
+    {
+        return $query->time > config('slow-query.slow_query_treshold_in_ms');
     }
 }
