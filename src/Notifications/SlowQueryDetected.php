860e6c46ddf1a22e8a596b0a6d1ae574f6e5897d
Mark van Eijk <mark@vaneijk.co>
2020-02-22 09:30:31 +0100

    slow query notification

---
+++ src/Notifications/SlowQueryDetected.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Vormkracht10\LaravelSlowQuery\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Http\Request;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Discord\DiscordChannel;
+use NotificationChannels\Discord\DiscordMessage;
+use Vormkracht10\LaravelSlowQuery\Query;
+
+class SlowQueryDetected extends Notification
+{
+    use Queueable;
+
+    public $deployment;
+
+    public function __construct(Query $query, Request $request)
+    {
+        $this->query = $query;
+        $this->request = $request;
+    }
+
+    public function via($notifiable)
+    {
+        return [DiscordChannel::class];
+    }
+
+    public function toDiscord($notifiable)
+    {
+        $message = array_random([
+            '☝🏻',
+        ]);
+
+        $message .= ' Slow query has been detected on **' . config('app.name') . '**';
+
+        return DiscordMessage::create($message, [
+            'title' => config('app.name'),
+            'color' => '3066993',
+            'fields' => [
+                [
+                    'name' => 'SQL',
+                    'value' => $this->query->sql,
+                    'inline' => false,
+                ], [
+                    'name' => 'Time run',
+                    'value' => $this->query->time,
+                    'inline' => false,
+                ],
+            ],
+            'url' => $this->request->fullUrlWithQuery(),
+        ]);
+    }
+}
